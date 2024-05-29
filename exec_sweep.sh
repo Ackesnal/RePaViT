7ffe9e37863c3d9865732cf3b87bb7b0f21eebdc
Ackesnal <ackesnal@foxmail.com>
2024-05-29 21:17:27 +1000

    updated

--- exec_sweep.sh
+++ exec_sweep.sh
@@ -6,14 +6,14 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --gres=gpu:tesla-smx2:1
 #SBATCH --mem-per-cpu=2G
-#SBATCH -o RePaViT_small_patch16_224_layer12_ChannelIdle_wandb_Sweep_out.txt
-#SBATCH -e RePaViT_small_patch16_224_layer12_ChannelIdle_wandb_Sweep_err.txt
+#SBATCH -o RePaViT_small_patch16_224_layer12_ChannelIdle+LayerScale_wandb_Sweep_out.txt
+#SBATCH -e RePaViT_small_patch16_224_layer12_ChannelIdle+LayerScale_wandb_Sweep_err.txt
 
 export WORLD_SIZE=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
-export MASTER_PORT=12584
+export MASTER_PORT=12585
 export MASTER_ADDR=$(scontrol show hostname $SLURM_NODELIST | head -n 1)
 
-WANDB_MODE=online srun python -m torch.distributed.launch --nproc_per_node=$SLURM_NTASKS_PER_NODE --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT --use_env train_sweep.py --data-path /scratch/itee/uqxxu16/data/imagenet --model RePaViT_small_patch16_224_layer12 --output_dir=output/sweep_optimization --feature_norm=BatchNorm --epochs=200 --channel_idle --use_wandb --wandb_no_loss --wandb_suffix=sweep --wandb_sweep_count=20
+WANDB_MODE=online srun python -m torch.distributed.launch --nproc_per_node=$SLURM_NTASKS_PER_NODE --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT --use_env train_sweep.py --data-path /scratch/itee/uqxxu16/data/imagenet --model RePaViT_small_patch16_224_layer12 --output_dir=output/sweep_optimization --feature_norm=BatchNorm --epochs=200 --channel_idle --use_wandb --wandb_no_loss --wandb_suffix=layer_scale_sweep --wandb_sweep_count=20 --layer_scale --wandb_sweep_id=vmkgxzbk
 
 # WANDB_MODE=[online, offline, disabled] -> 用于设定是否要联网同步
 # --use_wandb -> 使用wandb的功能，必须要加
