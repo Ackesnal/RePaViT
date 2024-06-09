085daddd0e51ea4a1ce3438a36f3844bff359e5b
Ackesnal <ackesnal@foxmail.com>
2024-06-09 22:03:41 +1000

    updated

--- exec_config.sh
+++ exec_config.sh
@@ -4,13 +4,13 @@
 #SBATCH --job-name=train
 #SBATCH --partition=gpu
 #SBATCH --cpus-per-task=32
-#SBATCH --gres=gpu:tesla-smx2:1
+#SBATCH --gres=gpu:1
 #SBATCH --mem-per-cpu=2G
-#SBATCH -o RePaViT_Large_patch16_224_layer12_out.txt
-#SBATCH -e RePaViT_Large_patch16_224_layer12_err.txt
+#SBATCH -o RePaViT_Tiny_patch16_224_layer12_out.txt
+#SBATCH -e RePaViT_Tiny_patch16_224_layer12_err.txt
 
 export WORLD_SIZE=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
-export MASTER_PORT=12556
+export MASTER_PORT=12580
 export MASTER_ADDR=$(scontrol show hostname $SLURM_NODELIST | head -n 1)
 
 #export BATCH_SIZE=$(echo "scale=0; 2048 / $WORLD_SIZE" | bc)
@@ -53,20 +53,39 @@ export MASTER_ADDR=$(scontrol show hostname $SLURM_NODELIST | head -n 1)
 #--wandb_suffix=full_300epoch
 
 
-export BATCH_SIZE=$(echo "scale=0; 1024 / $WORLD_SIZE" | bc)
-WANDB_MODE=online srun python -m torch.distributed.launch --nproc_per_node=$SLURM_NTASKS_PER_NODE --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT --use_env main.py --batch-size=$BATCH_SIZE --output_dir=output/RePaSwin_Tiny_patch16_224_layer12 --dist-eval \
---model=RePaSwin_Tiny_patch16_224_layer12 \
+#export BATCH_SIZE=$(echo "scale=0; 1024 / $WORLD_SIZE" | bc)
+#WANDB_MODE=online srun python -m torch.distributed.launch --nproc_per_node=$SLURM_NTASKS_PER_NODE --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT --use_env main.py --batch-size=$BATCH_SIZE --output_dir=output/RePaSwin_Tiny_patch16_224_layer12 --dist-eval \
+#--model=RePaSwin_Tiny_patch16_224_layer12 \
+#--data-path /scratch/itee/uqxxu16/data/imagenet \
+#--lr=1e-3 \
+#--min-lr=1e-5 \
+#--clip-grad=5.0 \
+#--warmup-lr=1e-6 \
+#--warmup-epochs=20 \
+#--unscale-lr \
+#--weight-decay=0.05 \
+#--opt=adamw \
+#--num_workers=30 \
+#--drop-path=0.2 \
+#--use_wandb \
+#--wandb_suffix=full_300epoch \
+#--color-jitter=0.4 \
+
+export BATCH_SIZE=$(echo "scale=0; 4096 / $WORLD_SIZE" | bc)
+WANDB_MODE=online srun python -m torch.distributed.launch --nproc_per_node=$SLURM_NTASKS_PER_NODE --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT --use_env main.py --batch-size=$BATCH_SIZE --output_dir=output/RePaViT_Tiny_patch16_224_layer12 --dist-eval \
+--model=RePaViT_Tiny_patch16_224_layer12 \
 --data-path /scratch/itee/uqxxu16/data/imagenet \
---lr=1e-3 \
+--feature_norm=BatchNorm \
+--lr=9e-3 \
 --min-lr=1e-5 \
---clip-grad=5.0 \
 --warmup-lr=1e-6 \
 --warmup-epochs=20 \
 --unscale-lr \
---weight-decay=0.05 \
---opt=adamw \
+--weight-decay=0.095 \
+--opt=lamb \
 --num_workers=30 \
---drop-path=0.2 \
+--channel_idle \
+--shortcut_gain=1.0 \
+--drop-path=0.05 \
 --use_wandb \
---wandb_suffix=full_300epoch \
---color-jitter=0.4 \
+--wandb_suffix=full_300epoch
